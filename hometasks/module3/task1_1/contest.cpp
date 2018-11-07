553f8e95ffb8963c504f7425e1eea064b4cc2eba
Ergy_MGC <ergy@MacBook-Air-Ergy-MGC.local>
2018-11-07 12:59:37 +0300

    Task 1_1: contest OK file

---
+++ hometasks/module3/task1_1/contest.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <stack>
+
+template<class T>
+class Tree
+{
+public:
+	Tree();
+
+	~Tree();
+
+	void Add( T key );
+
+	void PrintInOrder();
+
+private:
+	template<class K>
+	struct Node
+	{
+		K value;
+
+		Node<K>* left;
+		Node<K>* right;
+
+		Node(): left(nullptr), right(nullptr) {}
+	};
+
+	Node<T>* root;
+};
+
+//=====================//=====================//=====================//=====================//
+
+int main( int argc, char** argv )
+{
+	Tree<int> tree;
+
+	int count;
+	std::cin >> count;
+	for( int i = 0; i < count; ++i )
+	{
+		int input_num;
+		std::cin >> input_num;
+		tree.Add(input_num);
+	}
+
+	tree.PrintInOrder();
+	
+	return 0;
+}
+
+//=====================//=====================//=====================//=====================//
+
+template<class T>
+Tree<T>::Tree(): root(nullptr) {}
+
+template<class T>
+Tree<T>::~Tree()
+{
+	std::stack< Node<T>* > nodes;
+	
+	if( root != nullptr )
+	{
+		nodes.push( root );
+	}
+
+	while( !nodes.empty() )
+	{
+		Node<T>* tmp_node = nodes.top();
+		nodes.pop();
+
+		if( tmp_node->left != nullptr )
+		{
+			nodes.push( tmp_node->left );
+		}
+
+		if( tmp_node->right != nullptr )
+		{
+			nodes.push( tmp_node->right );
+		}
+
+		delete tmp_node;
+	}
+}
+
+template<class T>
+void Tree<T>::Add( T key )
+{
+	if( root == nullptr )
+	{
+		root = new Node<T>;
+		root->left = root->right = nullptr;
+		root->value = key;
+		return;
+	}
+
+	Node<T>* current_node = root;
+	while( true )
+	{
+		if( current_node->left == nullptr && key < current_node->value )
+		{
+			current_node->left = new Node<T>;
+			current_node->left->value = key;
+			return;
+		}
+		else if( current_node->right == nullptr && key >= current_node->value )
+		{
+			current_node->right = new Node<T>;
+			current_node->right->value = key;
+			return;
+		}
+		else if( current_node->left != nullptr && key < current_node->value )
+		{
+			current_node = current_node->left;
+		}
+		else if( current_node->right != nullptr && key >= current_node->value )
+		{
+			current_node = current_node->right;
+		}
+	}
+}
+
+template<class T>
+void Tree<T>::PrintInOrder()
+{
+	std::stack< Node<T>* > nodes;
+
+	Node<T>* current_node = root;
+	while( !nodes.empty() || current_node != nullptr )
+	{
+		if( current_node != nullptr )
+		{
+			nodes.push( current_node );
+			current_node = current_node->left;
+		}
+		else
+		{
+			current_node = nodes.top();
+			nodes.pop();
+
+			std::cout << current_node->value << " ";
+
+			current_node = current_node->right;
+		}
+	}
+}
