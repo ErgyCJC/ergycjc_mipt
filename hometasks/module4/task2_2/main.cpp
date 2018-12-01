f95f433f483d4199554cb9da0104ad553839faf9
Ergy_MGC <ergy@MacBook-Air-Ergy-MGC.local>
2018-12-01 19:03:23 +0300

    Task 2_2: first main commit

---
+++ hometasks/module4/task2_2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+struct Request
+{
+	int begin;
+	int end;
+
+	Request( int begin_, int end_ ) : begin( begin_ ), end( end_ ) {}
+};
+
+bool requestCompare()
+{
+	
+}
+
+int main( int argc, char** argv )
+{
+
+	return 0;
+}
