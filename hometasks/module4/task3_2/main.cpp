b2839d0af150d09fe21fb0d9d89d634b0534fc2d
Ergy_MGC <ergy@MacBook-Air-Ergy-MGC.local>
2018-12-06 05:21:20 +0300

    Task 3_2: code view

--- hometasks/module4/task3_2/main.cpp
+++ hometasks/module4/task3_2/main.cpp
@@ -1,3 +1,9 @@
+// Задача 3.2
+// Contest link: https://contest.yandex.ru/contest/10866/problems/3_2
+//
+// Дано N кубиков. Требуется определить каким количеством способов можно выстроить из этих кубиков пирамиду.
+// Каждый следующий уровень короче предыдущего.
+
 #include <iostream>
 #include <vector>
 
@@ -7,22 +13,26 @@ unsigned long long pyramidsVariantsCount( const int& count )
 		return 0;
 	}
 
+	// Кол-во способов построить пирамидки из n кубиков
 	std::vector< std::vector< unsigned long long > > ways( count + 1, std::vector<unsigned long long>( count + 1, 0 ) );
+	// Для любого кол-ва кубиков существует только один способ составить пирамидку из 0 кубиков
 	for( int i = 0; i < count + 1; ++i ){
 		ways[0][i] = 1;
 	}
 
-	for( size_t current_count = 1; current_count < count + 1; ++current_count ){
-		size_t bottom_layer;
+	// bottom_count - кол-во кубиков в нижнем слое
+	// using_count - используемое на данный момент кол-во кубиков для построения пирамидки
+	for( size_t bottom_count = 1; bottom_count < count + 1; ++bottom_count ){
+		size_t using_count;
 
-		for( bottom_layer = 1; bottom_layer <= current_count; ++bottom_layer ){
+		for( using_count = 1; using_count <= bottom_count; ++using_count ){
 			
-			ways[current_count][bottom_layer] = ways[current_count][bottom_layer - 1] +
-												ways[current_count - bottom_layer][bottom_layer - 1];		
+			ways[bottom_count][using_count] = ways[bottom_count][using_count - 1] +
+												ways[bottom_count - using_count][using_count - 1];		
 		}
 
-		for( ; bottom_layer < count + 1; ++bottom_layer ){
-			ways[current_count][bottom_layer] = ways[current_count][current_count];
+		for( ; using_count < count + 1; ++using_count ){
+			ways[bottom_count][using_count] = ways[bottom_count][bottom_count];
 		}
 	}
 	
