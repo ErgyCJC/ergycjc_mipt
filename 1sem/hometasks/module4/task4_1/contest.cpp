d874cbe1b53112ca5d2392fef2f4bb4e46daf50e
ErgyCjc <33071457+ErgyCJC@users.noreply.github.com>
2018-12-13 21:15:43 +0300

    Mod 4 Task 4_1: OK contest ver.

--- 1sem/hometasks/module4/task4_1/contest.cpp
+++ 1sem/hometasks/module4/task4_1/contest.cpp
@@ -19,10 +19,11 @@ private:
 
 	std::vector<int> heights;
 	std::vector<int> rootWays;
+	std::vector<int> parents;
 
-	void setHeights( int index, int par );
+	void setHeights( int index );
 
-	void setRootWays( int index, int par );
+	void setRootWays( int index );
 };
 
 //========================//========================//========================//========================//
@@ -33,31 +34,76 @@ Graph::Graph( int count )
 	
 	heights.resize( count, 0 );
 	rootWays.resize( count, 0 );
+	parents.resize( count, -1 );
 }
 
 void Graph::add( int index1, int index2 )
 {
 	connections[index1].push_back( index2 );
 	connections[index2].push_back( index1 );
+
+	parents[index2] = index1;	
 }
 
 void Graph::putOutDistances( std::ostream& out_stream )
 {
-	setHeights( 0, -1 );
-	setRootWays( 0, -1 );
+	setHeights( 0 );
+	setRootWays( 0 );
 
 	for( int i = 0; i < connections.size(); ++i ){
 		out_stream << std::max( rootWays[i], heights[i] ) << "\n";
 	}
 }
 
-void Graph::setHeights( int index, int par )
+void Graph::setHeights( int index )
 {
 	for( int i = 0; i < connections[index].size(); ++i ){
-		if( connections[index][i] != par ){
+		if( connections[index][i] != parents[index] ){
 			setHeights( connections[index][i] );
+
+			if( heights[ connections[index][i] ] + 1 > heights[index] ){
+				heights[index] = heights[ connections[index][i] ] + 1;
+			}
 		}
 	}
+}
+
+void Graph::setRootWays( int index )
+{
+	int maxSubHeight1 = -1, maxSubIndex1;
+    int maxSubHeight2 = -1, maxSubIndex2;
+
+    for( int i = 0; i < connections[index].size(); ++i ){
+		int next_index = connections[index][i];
+
+        if( next_index != parents[index] ){
+			if( maxSubHeight1 <= heights[next_index] + 1 ){
+				maxSubIndex2 = maxSubIndex1;
+				maxSubHeight2 = maxSubHeight1;
+				
+				maxSubIndex1 = next_index;
+				maxSubHeight1 = heights[next_index] + 1;
+			}
+			else if( maxSubHeight2 <= heights[next_index] + 1 ){
+				maxSubHeight2 = heights[next_index] + 1;
+				maxSubIndex2 = next_index;
+			}
+		}
+    }
+
+    for( int i = 0; i < connections[index].size(); ++i ){
+		int next_index = connections[index][i];
+
+		if( next_index != parents[index] ){
+			if (next_index == maxSubIndex1) {
+				rootWays[next_index] = std::max(rootWays[index], maxSubHeight2) + 1;
+			} else {
+				rootWays[next_index] = std::max(rootWays[index], maxSubHeight1) + 1;
+			}
+
+			setRootWays( next_index );
+		}
+    }
 }
 
 //========================//========================//========================//========================//
