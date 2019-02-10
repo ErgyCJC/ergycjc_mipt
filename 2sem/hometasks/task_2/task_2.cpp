1d32e48687128b145c112976fa9d0e2e8c8d01aa
ergy <ergy@MacBook-Pro-ergy.local>
2019-02-10 19:27:38 +0300

    task1_2: OK

--- 2sem/hometasks/task_2/task_2.cpp
+++ 2sem/hometasks/task_2/task_2.cpp
@@ -1,103 +1,72 @@
+// Task 1.2
+
+// Дан невзвешенный неориентированный граф. Найдите цикл минимальной длины.
+// В первой строке вводятся два натуральных числа N и M, не превосходящих 10000.
+// Далее идут M строк по 2 числа (от 0 до N-1) - индексы вершин между которыми есть ребро.
+// Требуется вывести одно целое число равное длине минимального цикла. Если цикла нет, то вывести -1.
+
+// Почему BFS, а не DFS?
+// Потому что DFS потребовал бы хранения дополнительной информации об обработанных потомках для каждоый вершины
+
 #include <vector>
 #include <iostream>
 #include <queue>
-#include <cmath>
 
-// Graph implementation - copy-paste from task 1.1
-struct IGraph {
+//=================//=================//=================//=================//
 
-    virtual ~IGraph() {}
-
-    virtual void AddEdge(int from, int to) = 0;
-
-    virtual int VerticesCount() const = 0;
-
-    virtual void GetNextVertices(int vertex,
-                                 std::vector<int> &vertices) const = 0;
-    virtual void GetPrevVertices(int vertex,
-                                 std::vector<int> &vertices) const = 0;
-};
-
-//============================//============================//============================
-
-class ListGraph : public IGraph {
+// Copy-paste graph implementation
+class ListGraph {
 public:
     ListGraph(int vertices_count);
+  
+    virtual ~ListGraph();
 
-    ListGraph(const IGraph* source_graph);
+    virtual void AddEdge(int from, int to);
 
-    virtual ~ListGraph() override;
-
-    virtual void AddEdge(int from, int to) override;
-
-    virtual int VerticesCount() const override;
+    virtual int VerticesCount() const;
 
     virtual void GetNextVertices(int vertex,
-                                 std::vector<int> &vertices) const override;
-    virtual void GetPrevVertices(int vertex,
-                                 std::vector<int> &vertices) const override;
+                               std::vector<int> &vertices) const;
 
 private:
-    std::vector< std::vector<int> > neighbours;
+  std::vector< std::vector<int> > neighbours;
 };
 
-ListGraph::ListGraph(int vertices_count) {
-    neighbours.resize(vertices_count);
-}
-
-ListGraph::ListGraph(const IGraph* source_graph) : ListGraph(source_graph->VerticesCount()) {
-    std::vector<int> children;
-
-    for (int i = 0; i < VerticesCount(); ++i) {
-        source_graph->GetNextVertices(i, children);
-        for (size_t v = 0; v < children.size(); ++v ) {
-            AddEdge(i, children[v]);
-        }
-    }
+ListGraph::ListGraph(int verticesCount) {
+  neighbours.resize(verticesCount);
 }
 
 ListGraph::~ListGraph() {}
 
 void ListGraph::AddEdge(int from, int to) {
-    neighbours[from].push_back(to);
+  neighbours[from].push_back(to);
 }
 
 int ListGraph::VerticesCount() const {
-    return static_cast<int>(neighbours.size());
+  return static_cast<int>(neighbours.size());
 }
 
 void ListGraph::GetNextVertices(int vertex, std::vector<int> &vertices) const {
-    vertices.clear();
-    vertices = neighbours[vertex];
+  vertices.clear();
+  vertices = neighbours[vertex];
 }
 
-void ListGraph::GetPrevVertices(int vertex, std::vector<int> &vertices) const {
-    vertices.clear();
+//=================//=================//=================//=================//
 
-    for (size_t v = 0; v < neighbours.size(); ++v) {
-        for (size_t i = 0; i < neighbours[v].size(); ++i) {
-            if (neighbours[v][i] == vertex) {
-                vertices.push_back(v);
-            }
-        }
-    }
-}
-
-//============================//============================//============================
-
-// Returns -1, if there are no 'visible' cycles from the root_vertex cycles, or the length of the shortest one
-int bfs_cycle_search(const ListGraph& graph, const int root_v) {
-    std::vector<int> parent(graph.VerticesCount());
-    std::vector<int> depth(graph.VerticesCount(), 0);
+int CycleSearch(int root_v, ListGraph& graph) {
     std::vector<bool> visited(graph.VerticesCount(), false);
-    
     visited[root_v] = true;
+
+    // Contains distances between root_v and other vertices
+    std::vector<int> depth(graph.VerticesCount(), 0);
+    
+    // Contains parents of push'ed into bfs-queue vertices
+    std::vector<int> parent(graph.VerticesCount());
 
     std::queue<int> bfs_queue;
     bfs_queue.push(root_v);
 
-    int result = -1;
-
+    // BFS
     while (!bfs_queue.empty()) {
         int current_v = bfs_queue.front();
         bfs_queue.pop();
@@ -106,55 +75,50 @@ int bfs_cycle_search(const ListGraph& graph, const int root_v) {
         graph.GetNextVertices(current_v, children);
 
         for (auto v : children) {
-            if (!visited[v]) {
+            if(!visited[v]){ // Going into a new verex
                 bfs_queue.push(v);
-                
+
                 parent[v] = current_v;
-                visited[v] = true;
                 depth[v] = depth[current_v] + 1;
+                visited[v] = true;
             }
-            else if (v != parent[current_v]) {
-                result = depth[current_v] + depth[v] + 1;
-                break;
+            // Visited (grey) vertex found;
+            // It's a common cycle vertex if it isn't the parent of the current vertex
+            else if(v != parent[current_v]){ 
+                return  depth[v] + depth[current_v] + 1;
             }
         }
     }
 
-    return result;
+    return graph.VerticesCount() + 1;
 }
 
-// Returns -1, if there are no cycles in graph, or the length of the shortest one
-int min_cycle_length(const ListGraph& graph) {
-    std::vector<bool> visited(graph.VerticesCount(), false);
-    int min_cycle_length = -1;
+// Returns min cycle length or -1 if it doesn't exist
+int GetMinCycle(ListGraph& graph) {
+    int min_cycle_length = graph.VerticesCount() + 1;
 
     for (int i = 0; i < graph.VerticesCount(); ++i) {
-        int cycle_length = bfs_cycle_search(graph, i);
-        if (min_cycle_length == -1 || cycle_length < min_cycle_length) {
-            min_cycle_length = cycle_length;
-        }
+        min_cycle_length = std::min(CycleSearch(i, graph), min_cycle_length);
     }
 
-    return min_cycle_length;
+    return (min_cycle_length == graph.VerticesCount() + 1 ? -1 : min_cycle_length);
 }
 
-//============================//============================//============================
+//=================//=================//=================//=================//
 
 int main(int argc, char** argv) {
-    int vertices_count, edges_count;
+    int v_count, edges_count, from, to;
 
-    std::cin >> vertices_count >> edges_count;
-    ListGraph graph(vertices_count);
+    std::cin >> v_count >> edges_count;
+    ListGraph graph(v_count);
 
     for (int i = 0; i < edges_count; ++i) {
-        int from, to;
         std::cin >> from >> to;
-
+        
         graph.AddEdge(from, to);
         graph.AddEdge(to, from);
     }
 
-    std::cout << min_cycle_length(graph);
-
+    std::cout << GetMinCycle(graph) << "\n";
     return 0;
 }
