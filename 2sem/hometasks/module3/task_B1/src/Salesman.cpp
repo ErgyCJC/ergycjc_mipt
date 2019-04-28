1194736586524d77100703c0b8d9bdc2ae6f5cbd
ergy <ergycjc@gmail.com>
2019-04-28 16:52:40 +0300

    task B1 comments

--- 2sem/hometasks/module3/task_B1/src/Salesman.cpp
+++ 2sem/hometasks/module3/task_B1/src/Salesman.cpp
@@ -3,18 +3,21 @@
 SalesmanSolver::SalesmanSolver(Graph graph) : base_graph(graph) {}
 
 double SalesmanSolver::FindPathCost() {
+    // Постоение MST
     std::vector<Edge> mst = BuildMST(base_graph);
     std::vector<bool> visited_vertices(base_graph.VCount());
 
     double path_cost = 0.0;
     int last_vertex;
 
+    // Выбор начального ребра в цикле
     if (mst.size() > 0) {
         last_vertex = mst[0].to_vertex;
         path_cost = mst[0].weight;
         visited_vertices[mst[0].to_vertex] = visited_vertices[mst[0].from_vertex] = true;
     }
 
+    // Проход по вершинам из MST
     for (int i = 1; i < mst.size(); ++i) {
         Edge& curr_edge = mst[i];
         if (!visited_vertices[curr_edge.from_vertex]) {
